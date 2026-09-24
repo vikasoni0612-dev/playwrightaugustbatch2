e6a8af811e3f331b431c0c74312f13425bd70329
Vikas Soni <vikasoni0612@gmail.com>
2026-09-24 18:04:50 +0530

    updating the test2 changes

--- tests/basic.spec.js
+++ tests/basic.spec.js
@@ -1,6 +1,27 @@
 import {test} from '@playwright/test';   
 
-test('login',async function testplaywright({browser}){
+test('login',async({browser})=>{
+    //fisxture: ready to use setup provided by playwright for test
+    //types of fixctures;
+    /*
+    broweser
+    context
+    page
+    request
+    */
+   const context=await browser.newContext();
+   const page=await context.newPage();
+   await page.goto("https://www.automationpracticehub.com/");
+   //usernam
+   //password
+   //login button click
+   await page.waitForTimeout(6000);
+
+    });
+
+
+
+test('signin',async({browser})=>{
     //fisxture: ready to use setup provided by playwright for test
     //types of fixctures;
     /*
